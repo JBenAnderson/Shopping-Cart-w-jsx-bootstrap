7215aa0c9626088e1d4b25c30a261420ba885edc
J Ben Anderson <revovisions@gmail.com>
2021-04-27 17:03:20 -0400

    created index.jsx, applied initial cart logic

---
+++ index.jsx
@@ -0,0 +1,17 @@
+function NavBar({menuitems, minstock}) {
+ //first list filters items by their 'instock' number if greater than minstock number
+  let list1 = menuitems.filter(item => item.instock >= minstock);
+  
+  let list2 = list1.map((item, index) => {
+    return <li key={index.toString()}>{item.name}</li>;
+  });
+
+  return <ul style={{ listStyleType: "none" }}>{list2}</ul>;
+
+}
+//Our Array of objects with name of fruit and quantity instock
+const menuItems = [{name: 'apple', instock: 2 }, {name: 'pineapple', instock: 3 }, 
+{name: 'pear', instock: 0 }, {name: 'peach', instock: 3 }, {name: 'orange', instock: 1 }
+];
+//Render to the ReactDOM what menuItems are greater than minstock number
+ReactDom.render(<NavBar menuitems={menuItems} minstock={2} />, document.getElementById("root"));
